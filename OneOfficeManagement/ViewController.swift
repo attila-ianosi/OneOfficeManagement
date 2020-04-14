432c5fd14d0cf71c37411cbaf34641a19cc20298
Attila Ianosi <attila.ianosi@icloud.com>
2020-04-14 14:05:49 +0100

    Initial Commit

---
+++ OneOfficeManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OneOfficeManagement
+//
+//  Created by Attila I on 14/04/2020.
+//  Copyright © 2020 Attila I. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
